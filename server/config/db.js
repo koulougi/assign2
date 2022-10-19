829f0f812c3bc0bc85ca21ef12da5f505f18c5b6
koulougi <112962767+koulougi@users.noreply.github.com>
2022-10-19 14:05:01 -0400

    updated db uri to work with mongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI": "mongodb://localhost/user_collect"
+    //"URI": "mongodb://localhost/user_collect"
+    "URI": "mongodb+srv://koulougi:yPR1K1h2NGyjKpP9@dbserver.ohk6eum.mongodb.net/?retryWrites=true&w=majority"
 }
